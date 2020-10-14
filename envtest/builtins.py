f0d7855ec58cf29127c0aa1f8e5240815105c4cf
Zhijin Zhang <zhangzhijing1995@gmail.com>
2020-10-14 10:40:52 +0100

    Add pandas based function.

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -1,8 +1,10 @@
 import numpy as np
+import pandas as pd
 from scipy.ndimage import gaussian_filter
 from scipy import misc
 
-__all__ = ['rand_array', 'smooth_image', 'my_mat_solve']
+# These functions are made visible
+__all__ = ['rand_array', 'smooth_image', 'my_mat_solve', 'days_forward']
 
 
 def rand_array(shape):
@@ -13,3 +15,6 @@ def smooth_image(a, sigma=1):
 
 def my_mat_solve(A, b):
     return A.inv()*b
+
+def days_forward(start, days):
+    return pd.date_range(start, periods=days)
